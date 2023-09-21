eed44f444f18c06b28fe08e0de5738091b8f84b3
Florent Uzio <fuzio@ripple.com>
2023-09-21 10:37:37 +0400

    frontend

---
+++ apps/lending/frontend/src/pages/home.page.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mantine/core"
+
+export const HomePage = () => {
+  return <Container>Test</Container>
+}
---
+++ apps/lending/frontend/src/shared/contexts/web3-context.tsx
@@ -0,0 +1,59 @@
+import { createContext, useContext, useEffect, useState } from "react"
+
+type Web3ContextApi = {
+  connectWallet: () => void
+}
+
+const SMART_CONTRACT_ADDRESS = import.meta.env.VITE_SMART_CONTRACT_ADDRESS
+
+export const Web3Context = createContext<Web3ContextApi>({} as Web3ContextApi)
+
+type Web3ProviderProps = {
+  children: React.ReactNode
+}
+
+export const Web3Provider: React.FC<Web3ProviderProps> = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState("")
+
+  const checkIfWalletIsConnected = async () => {
+    if (!window.ethereum) {
+      return alert("Please install Metamask")
+    }
+
+    const accounts = await window.ethereum.request({ method: "eth_accounts" })
+
+    if (accounts.length) {
+      setCurrentAccount(accounts[0])
+    } else {
+      console.log("No accounts found.")
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  }, [])
+
+  const connectWallet = async () => {
+    if (!window.ethereum) {
+      return alert("Please install Metamask")
+    }
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    })
+
+    setCurrentAccount(accounts[0])
+
+    window.location.reload()
+  }
+
+  return <Web3Context.Provider value={{ connectWallet }}>{children}</Web3Context.Provider>
+}
+
+export const useWeb3 = () => {
+  const context = useContext(Web3Context)
+  if (!context) {
+    throw new Error("useWeb3 must be used inside Web3Provider")
+  }
+  return context
+}
