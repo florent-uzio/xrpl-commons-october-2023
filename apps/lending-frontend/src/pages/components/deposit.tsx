83c193937cbedbf2bd0e5e69ccbc179fc63d0485
Florent Uzio <fuzio@ripple.com>
2023-09-25 13:18:39 +0400

    deposit component updated

--- apps/lending-frontend/src/pages/components/deposit.tsx
+++ apps/lending-frontend/src/pages/components/deposit.tsx
@@ -11,23 +11,55 @@ import {
   InputGroup,
   InputLeftElement,
   Text,
+  useToast,
 } from "@chakra-ui/react"
 import { ethers } from "ethers"
+import { useEffect } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
+import { TypedListener } from "../../contracts/common"
 import { useContract } from "../../shared/hooks"
 
 type DepositForm = {
   amount: string
 }
 
+/**
+ * Component that renders a Card and the input to deposit XRP into the contract.
+ *
+ * @returns
+ */
 export const Deposit = () => {
   const { register, handleSubmit, watch } = useForm<DepositForm>()
+  // get the contract
   const contract = useContract()
+  const toast = useToast()
 
+  // what happens when you click on the button
   const onSubmit: SubmitHandler<DepositForm> = async ({ amount }) => {
     if (!contract) return
     await contract.deposit({ value: ethers.parseEther(amount) })
   }
+
+  useEffect(() => {
+    if (!contract) return
+    const event = contract.getEvent("Deposited")
+
+    const showToast: TypedListener<typeof event> = (user, amount) => {
+      toast({
+        title: "Deposit Successful",
+        description: `${ethers.formatUnits(amount.toString(), "ether")} XRP deposited by ${user}`,
+        status: "success",
+        isClosable: true,
+      })
+    }
+
+    // show a toast upon receipt of the Deposited event
+    contract.on(event, showToast)
+
+    return () => {
+      contract.removeListener(event, showToast)
+    }
+  }, [contract])
 
   return (
     <Card>
