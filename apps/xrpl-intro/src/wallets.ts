48874c6ff7d7105ba40798f4866947fa80331006
Florent Uzio <fuzio@ripple.com>
2023-10-12 11:12:03 +0400

    wallet updates

--- apps/xrpl-intro/src/wallets.ts
+++ apps/xrpl-intro/src/wallets.ts
@@ -1,10 +1,4 @@
-import { Wallet } from "xrpl"
-import "dotenv/config"
+// todo: create and export the wallets from .env
 
-const SEED_1 = process.env.SEED_1 ?? ""
-const SEED_2 = process.env.SEED_2 ?? ""
-const SEED_3 = process.env.SEED_3 ?? ""
-
-export const WALLET_1 = Wallet.fromSeed(SEED_1)
-export const WALLET_2 = Wallet.fromSeed(SEED_2)
-export const WALLET_3 = Wallet.fromSeed(SEED_3)
+// import { Wallet } from "xrpl"
+// import "dotenv/config"
