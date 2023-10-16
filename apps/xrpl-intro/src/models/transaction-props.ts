d96b705beda84835abc92beaf69cd09927790ec6
Florent Uzio <fuzio@ripple.com>
2023-10-16 10:38:41 +0400

    create nft offer

--- apps/xrpl-intro/src/models/transaction-props.ts
+++ apps/xrpl-intro/src/models/transaction-props.ts
@@ -1,11 +1,20 @@
-import { Transaction } from "xrpl"
+import { NFTokenCreateOffer, NFTokenCreateOfferFlags, Transaction } from "xrpl"
 import { TxnOptions } from "./txn-options"
 
 export type TransactionPropsForMultiSign = TxnOptions & {
   isMultisign: true
 }
 
+// Props for the NFTokenCreateOffer transaction type
+type NFTokenCreateOfferProps<T extends Transaction> = Omit<T, "TransactionType" | "Account"> &
+  (
+    | { Flags: NFTokenCreateOfferFlags.tfSellNFToken; Owner?: never }
+    | { Flags?: undefined; Owner: string }
+  )
+
 export type TransactionPropsForSingleSign<T extends Transaction> = TxnOptions & {
   isMultisign?: false
-  txn: Omit<T, "TransactionType" | "Account">
+  txn: T extends NFTokenCreateOffer
+    ? NFTokenCreateOfferProps<T>
+    : Omit<T, "TransactionType" | "Account">
 }
