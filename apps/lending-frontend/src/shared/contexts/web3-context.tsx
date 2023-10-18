44d50574da8b5601673101e7af206fa6021f655a
Florent Uzio <fuzio@ripple.com>
2023-10-18 16:21:16 +0400

    check account ownership

--- apps/lending-frontend/src/shared/contexts/web3-context.tsx
+++ apps/lending-frontend/src/shared/contexts/web3-context.tsx
@@ -8,6 +8,7 @@ export type Web3ContextApi = {
   connectWallet: () => void
   contract: SimpleBank | null
   disconnect: () => void
+  isOwner: boolean
   signer: JsonRpcSigner | null
 }
 
@@ -21,6 +22,7 @@ export const Web3Provider: FC<Props> = ({ children }) => {
   const [account, setAccount] = useState("")
   const [contract, setContract] = useState<SimpleBank | null>(null)
   const [signer, setSigner] = useState<JsonRpcSigner | null>(null)
+  const [isOwner, setIsOwner] = useState(false)
 
   const { ethereum } = window
 
@@ -41,6 +43,10 @@ export const Web3Provider: FC<Props> = ({ children }) => {
     // Set the contract using the signer
     const contractInstance = SimpleBank__factory.connect(SimpleBankJson.address, signer)
     setContract(contractInstance)
+
+    // Check if the connected metamask address is the one that deployed the contract
+    const owner = await contractInstance.owner()
+    setIsOwner(owner === signer.address)
   }
 
   const disconnect = () => {
@@ -63,6 +69,7 @@ export const Web3Provider: FC<Props> = ({ children }) => {
     connectWallet,
     contract,
     disconnect,
+    isOwner,
     signer,
   }
 
