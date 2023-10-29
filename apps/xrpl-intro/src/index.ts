0ec8cf53c14f7acd96219fd570a1ce95877cdab0
Florent Uzio <fuzio@ripple.com>
2023-10-29 15:10:59 +0100

    removed some code

--- apps/xrpl-intro/src/index.ts
+++ apps/xrpl-intro/src/index.ts
@@ -1,7 +1,5 @@
 import "dotenv/config"
 import { getXrplClient } from "./client"
-import { cancelCheck } from "./transactions"
-import { WALLET_2 } from "./wallets"
 
 // const pinataClient = getPinataClient()
 const xrplClient = getXrplClient()
@@ -26,12 +24,12 @@ const main = async () => {
 
   // console.log(JSON.stringify(res, null, 2))
 
-  await cancelCheck({
-    txn: {
-      CheckID: "E3EA5C75A22A679F55F0EC6244FB645226747C5FFDAC7AB7E199CD08CC366A42",
-    },
-    wallet: WALLET_2,
-  })
+  // await cancelCheck({
+  //   txn: {
+  //     CheckID: "E3EA5C75A22A679F55F0EC6244FB645226747C5FFDAC7AB7E199CD08CC366A42",
+  //   },
+  //   wallet: WALLET_2,
+  // })
 
   // await mintNft({
   //   txn: {
@@ -43,47 +41,5 @@ const main = async () => {
 
   await xrplClient.disconnect()
 }
-
-// const getPageNumbers = async () => {
-//   await xrplClient.connect()
-
-//   let res = await xrplClient.request({
-//     command: "account_objects",
-//     account: "rQLpRRwknu51LornBtpZLWGoK8UCEMFX3v",
-//     type: "nft_page",
-//   })
-
-//   let count = res.result.account_objects.length
-
-//   while (res.result.marker) {
-//     res = await xrplClient.request({
-//       command: "account_objects",
-//       account: "rQLpRRwknu51LornBtpZLWGoK8UCEMFX3v",
-//       type: "nft_page",
-//       marker: res.result.marker,
-//     })
-
-//     count += res.result.account_objects.length
-//   }
-
-//   console.log(count)
-
-//   await xrplClient.disconnect()
-// }
-
-// const edge = async () => {
-//   await xrplClient.connect()
-//   const tx = await xrplClient.request({
-//     command: "tx",
-//     transaction: "8821A8EF3E1BC04B59FC2C4056EDC6C8440BF6E40B231D810936C159953A44E4",
-//   })
-
-//   if (tx.result.meta && typeof tx.result.meta === "object") {
-//     const res = getBalanceChanges(tx.result.meta)
-//     console.log(JSON.stringify(res, null, 2))
-//   }
-
-//   await xrplClient.disconnect()
-// }
 
 main()
